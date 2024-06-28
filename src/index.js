f2a7668b3015fb69e92ce5060966f09c209194f4
红烧猎人 <95302870@qq.com>
2024-06-28 09:24:56 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.sun-panel.top": dockerHub,
+  "quay.sun-panel.top": "https://quay.io",
+  "gcr.sun-panel.top": "https://gcr.io",
+  "k8s-gcr.sun-panel.top": "https://k8s.gcr.io",
+  "k8s.sun-panel.top": "https://registry.k8s.io",
+  "ghcr.sun-panel.top": "https://ghcr.io",
+  "cloudsmith.sun-panel.top": "https://docker.cloudsmith.io",
+  "ecr.sun-panel.top": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.sun-panel.top": dockerHub,
 };
 
 function routeByHosts(host) {
